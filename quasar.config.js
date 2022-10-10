a59a6aaf0faa5b32c42e8b900a719de74a2e2b89
vladimir Demyanchuk <wovka.home@gmail.com>
2022-10-11 04:08:23 +0500

    праввки

--- quasar.config.js
+++ quasar.config.js
@@ -74,16 +74,6 @@ module.exports = configure(function (/* ctx */) {
       // viteVuePluginOptions: {},
 
       vitePlugins: [
-        [
-          '@intlify/vite-plugin-vue-i18n',
-          {
-            // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-            // compositionOnly: false,
-
-            // you need to set i18n resource including paths !
-            include: path.resolve(__dirname, './src/i18n/**'),
-          },
-        ],
       ],
     },
 
